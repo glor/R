160f3c9a0f10af854b7a79dbbefba87ec0e8fe2f
DorotheaMueller <dorothea.mueller01@stud.uni-goettingen.de>
2016-03-08 15:43:12 +0100

    Update 1.r

--- aufgaben/blatt03/1.r
+++ aufgaben/blatt03/1.r
@@ -13,16 +13,22 @@ boxplot(maeuse$speed ~ maeuse$health, date = maeuse, main = "kranke Maeuse" )
 
 #1.4
 #Geeignetes Hypothesenpaar:
-#Nullhypothese: Die gesunde Gruppe ist schneller oder gleich schnell wie die kranke. H0: healthy >= sick
-#Alternativhypothese: Die gesunde Gruppe ist langsamer als die gesunde. H1: healthy < sick
+
+# H1: Die gesunden Mauese waren schneller.
+# H0: Die gesunden Mauese waren gleich schnell oder langsamer.
+
+(#Nullhypothese: Die gesunde Gruppe ist schneller oder gleich schnell wie die kranke. H0: healthy >= sick
+#Alternativhypothese: Die gesunde Gruppe ist langsamer als die gesunde. H1: healthy < sick)
+
 
 #1.5
 #Vorraussetzungen t-Test: Varianzhomogenitaet, Normalverteilung, Unabhaengigkeit
+# keine Var.homogen. --> t-Welch
 # Varianzunabh. und Normalverteilung s.o.
 # Unabhaengigkeit der Daten ist ausserdem auch gegeben.
 #t-Welch wird bei Invarianz durchgefuehrt, der klassische t-Test bei Varianzhomogenitaet
 
 #1.6
 t.test(formula=speed~health, data=maeuse, var.equal=TRUE, alternative="less", conf.level=0.95)
-#p-Wert:
-#Konfidenzintervalle 
+#p-Wert:# p-Wert von 2.376e-05 kleiner als 0.05, d.h. es gibt einen signifikanten Unterschied —> Nullhypothese kann abgelehnt werden, die kranken Mäuse brauchen länger
+#Konfidenzintervalle # 95% Intervall —> in 95% der so ermittelten Fälle ist Unterschied zwischen den Populationen erkennbar
