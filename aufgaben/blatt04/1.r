ce6d11cbad1f9ea64b6e029a209cfda2983edc81
sonnengeschein <sonnengeschein@users.noreply.github.com>
2016-03-08 16:39:47 +0100

    Update 1.r

--- aufgaben/blatt04/1.r
+++ aufgaben/blatt04/1.r
@@ -1,2 +1,21 @@
-#4.2
-t.test(formula = hair$length_difference ~ hair$type, var.eqaul = TRUE, alternative = "two.sided", conf.level = 0.95, paired = TRUE)
+#Blatt 4
+
+#1.1 Runterladen
+
+#1.2
+hefe = read.table(file="[017]yeast.txt", sep="t", dec=".", header=TRUE)
+
+#1.3
+boxplot(hefe$content ~ hefe$type, data = hefe, main = "hefe" )
+#Keine Normalverteilung, da es in jedem Typ einen Ausreisser gibt
+#Varianzhomogenitaet, da die Unterschiede zwischen den beiden Boxen noch im Rahmen sind
+#Wilcoxon-Test: braucht keine Normalverteilung, nur Varianzhomogenitaet und unabhaengige Daten (hier wurden verschiedene Kulturen untersucht, dadurch gegeben)
+
+#1.4
+#H1: mutant > control
+#H0: mutant <= control
+
+#1.5
+wilcox.test(formula=content~type, data=hefe,alternative="less",conf.level=0.95,dist="exact")
+#p-value = 0.0004667
+#Da der p-Wert kleiner als 5% ist, kann die Alternativhypothese angenommen werden, also dass die mutierten Hefen tatsaechlich mehr von der Fettsaeure produzieren.
