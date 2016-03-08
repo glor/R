bfdbf97cdf2f2cdd9b3c3e986595de1e51948d80
glor <glor@live.de>
2016-03-08 16:02:24 +0100

    Create 2.r

---
+++ aufgaben/blatt03/2.r
@@ -0,0 +1,5 @@
+#3 Aufgabe 2:
+#3.1
+laus = read.table(file = "lice.txt", sep="\t", dec = ".", header = TRUE)
+# H1: Kita2 hat weniger Kopflaeuse gehabt
+# H0: Kita2 hat gleich viel oder mehr Kopflaeuse als Kita1 gehabt
