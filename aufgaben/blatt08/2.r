6d1c36aedcd89f6ce37d5ad347253a482730efbb
sonnengeschein <sonnengeschein@users.noreply.github.com>
2016-03-10 18:19:37 +0100

    Update 2.r

--- aufgaben/blatt08/2.r
+++ aufgaben/blatt08/2.r
@@ -1,3 +1,44 @@
+#Blatt 8
+
+#2
+step = read.table(file="[013]stepping.txt", dec=".", sep="t", header = TRUE )
+
+#Varianzhomogenitaet
+model = lm(formula=HR ~ RestHR + Block + Height + Frequency + (Height:Frequecy), data = step)
+fitted.value=fitted(model)
+resid.value=resid(model)
+plot(fitted.value, resid.value)
+abline(h=0)
+# keine Regelmaessigkeiten erkennbar => varianzhomogen
+
+boxplot(resid.value)
+#Ausreisser: dennoch weiter machen, auch wenn nicht normalverteilt
+
+#Hypothesen aufstellen:
+#A Schritthoehe
+#B: Schrittfrequenz
+#C: RuheHR
+#H0A: (height=1) = (height=0)
+#H1A:(height=1) != (height=0)
+#H0B.1: (Frequency = 0) = (Frequency = 1)
+#H0B.2: (Frequency = 2) = (Frequency = 1)
+#H0B.3: (Frequency = 0) = (Frequency = 2)
+#H1B.1: (Frequency = 0) = (Frequency = 1)
+#H1B.2: (Frequency = 0) = (Frequency = 2)
+#H1B.3: (Frequency = 2) = (Frequency = 1)
+#H0C: RestHR aehnlich (Unterschied <= +/- 5)
+#H1C: RestHR sehr unterschiedlich (Unterschied > +/- 5)
+#H0W: keine Wechselwirkungen
+#H1W: Wechselwirkungen
+
+anova(model)
+#Signifikante p-Werte (und hohe F-Werte): RestHR: 3.311e-05
+# Height: 0.0007625
+# Frequency: 1.297e-05
+#Also korrelieren diese Werte mit dem HR. Die restlichen nicht. 
+
+
+
 cm = lm(formula=response~treatment.A+treatment.B+ treatment.A:treatment.B, data + cherry)
 fitted.value=fitted(cm)
 resid.value=resid(cm)
